92e7ec74300662d2338ffb778a1627b38fb4cd5f
blade-demon <xuziwei89@gmail.com>
2022-12-15 07:52:51 +0800

    add frontend files

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+// enable body parser
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: false,
   })
 );
+
+// set public static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/openai", require("./routes/openaiRoutes"));
 app.listen(port, () => console.log(`Server started on port ${port}`));
